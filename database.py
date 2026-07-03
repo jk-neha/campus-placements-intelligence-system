4a48a49ab1adab0688e7b3856cee0110717bbfc2
Neha <jknehvardhini222106619@gmail.com>
2026-07-03 20:01:48 +0530

    Fix CORS for Vercel frontend

--- database.py
+++ database.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 
 
+from dotenv import load_dotenv
 import os
 
-DATABASE_URL=os.getenv("DATABASE_URL")
+load_dotenv()
 
-engine=create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 Session_Local=sessionmaker(bind=engine)
 Base=declarative_base()
